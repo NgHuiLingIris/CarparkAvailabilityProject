443719c0d751ab17cd26de47c5338bcb3e679316
Ng Hui Ling Iris <irisnghl@gmail.com>
2023-03-03 02:33:33 +0800

    add availability

--- app.py
+++ app.py
@@ -18,13 +18,21 @@ def results():
     DATE_TIME = now.strftime('%Y-%m-%dT%H:%M:%S')  # Format current datetime as string
     # query Carpark Availability API and parse CSV file
     
-    # this is not important now
-    # get_carpark_data(DATE_TIME)['items'][0]['carpark_data']
     carpark_data = parse_carpark_csv()
 
     # calculate distances and filter to top 3 carparks
     carpark_data = calculate_distances(user_lat, user_lng, carpark_data)
     top_carparks = get_top_carparks(carpark_data)
+
+    # Get current carpark availability data
+    current_carpark_availability = get_carpark_data(DATE_TIME)['items'][0]['carpark_data']
+
+    # Append carpark availability to each top carpark
+    for carpark in top_carparks:
+        carpark_number = carpark['car_park_no']
+        for cp_data in current_carpark_availability:
+            if cp_data['carpark_number'] == carpark_number:
+                carpark['availability'] = cp_data['carpark_info']
 
     # render template with top 3 carparks
     return render_template('results.html', carparks=top_carparks)
