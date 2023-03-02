59f813d1ad2addc3200787ea19f6da3d123ddf7a
Iris Ng <irisnghl@gmail.com>
2023-03-02 19:33:52 +0800

    update geolocation

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from utils import get_carpark_data, parse_carpark_csv, calculate_distances, get_top_carparks
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
+@app.route('/results')
+def results():
+    # get user's location from AJAX request
+    user_lat = request.args.get('lat')
+    user_lng = request.args.get('lng')
+
+    now = datetime.datetime.now()
+    DATE_TIME = now.strftime('%Y-%m-%dT%H:%M:%S')  # Format current datetime as string
+    # query Carpark Availability API and parse CSV file
+    
+    # this is not important now
+    # get_carpark_data(DATE_TIME)['items'][0]['carpark_data']
+    carpark_data = parse_carpark_csv()
+
+    # calculate distances and filter to top 3 carparks
+    carpark_data = calculate_distances(user_lat, user_lng, carpark_data)
+    top_carparks = get_top_carparks(carpark_data)
+
+    # render template with top 3 carparks
+    return render_template('results.html', carparks=top_carparks)
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,44 @@
+import requests
+import csv
+import geopy.distance
+
+# Define API endpoint and parameters
+API_ENDPOINT = 'https://api.data.gov.sg/v1/transport/carpark-availability'
+carpark_data = None  # Initialize the in-memory data to None
+# Define function to get carpark availability data
+def get_carpark_data(date_time):
+    params = {'date_time': date_time}
+    response = requests.get(API_ENDPOINT, params=params)
+    data = response.json()
+    return data
+
+def parse_carpark_csv():
+    global carpark_data  # Use the global carpark_data variable
+
+    # If the carpark data is already in memory, return it
+    if carpark_data is not None:
+        return carpark_data
+
+    # Otherwise, read the CSV file and store the data in memory
+    with open('data/hdb_carpark_information_wgs84.csv') as f:
+        reader = csv.DictReader(f)
+        carpark_data = [row for row in reader]
+    return carpark_data
+
+def calculate_distances(user_lat, user_lng, carpark_data):
+    # calculate distances to each carpark and add to list of dictionaries
+    for carpark in carpark_data:
+        lat = float(carpark['lat'])
+        lng = float(carpark['lon'])
+        coords = (lat, lng)
+        distance = geopy.distance.distance((user_lat, user_lng), coords).km
+        carpark['distance'] = distance
+    return carpark_data
+
+def get_top_carparks(carpark_data, num_carparks=3):
+    # sort carpark list by distance
+    sorted_carparks = sorted(carpark_data, key=lambda x: x['distance'])
+
+    # filter list to top N carparks
+    top_carparks = sorted_carparks[:num_carparks]
+    return top_carparks
